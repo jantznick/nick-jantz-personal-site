941c1f41ed153b399c70e2f85b45e2a9730d5c7e
Nick Jantz <thenickjantz@gmail.com>
2020-06-10 21:54:26 -0500

    add some views

--- app.js
+++ app.js
@@ -6,9 +6,52 @@ const port = 3000;
 app.set("view engine","ejs");
 app.use(express.static("public"));
 
+const menu = [
+	{
+		name: 'Home',
+		link: '/'
+	},
+	{
+		name: 'Resume',
+		link: '/resume'
+	},
+	{
+		name: 'Contact',
+		link: '/contact'
+	}
+]
+
 app.get("/", (req,res) => {
-	res.render('index');
+	res.render('index', {
+		activePage: 'home',
+		menu: menu
+	});
 });
+
+app.get("/resume", (req,res) => {
+	res.render('resume', {
+		activePage: 'resume',
+		menu: menu
+	});
+})
+
+app.get("/contact", (req,res) => {
+	res.render('contact', {
+		activePage: 'contact',
+		menu: menu
+	});
+})
+
+app.get("/login", (req,res) => {
+	res.render('login', {
+		activePage: 'login',
+		menu: menu
+	});
+})
+
+app.get("*", (req,res) => {
+	res.render('error')
+})
 
 app.listen(port, () => {
 	console.log(`SERVER IS RUNNING ON PORT ${port}!`);

9fb648a3cfe2331b1a002d15b7a65537781fba7a
Nick Jantz <thenickjantz@gmail.com>
2021-01-03 22:00:58 -0600

    update config info

--- app.js
+++ app.js
@@ -1,53 +1,63 @@
 const express = require('express');
 const app = express();
-
 const port = 3006;
+
+require('dotenv').config()
 
 app.set("view engine","ejs");
 app.use(express.static("public"));
 
 
 const menu = [
-	// {
-	// 	name: 'Home',
-	// 	link: '/'
-	// },
-	// {
-	// 	name: 'Resume',
-	// 	link: '/resume'
-	// },
-	// {
-	// 	name: 'Contact',
-	// 	link: '/contact'
-	// }
+	{
+		name: 'Home',
+		link: '/'
+	},
+	{
+		name: 'Resume',
+		link: '/resume'
+	},
+	{
+		name: 'Contact',
+		link: '/contact'
+	}
 ]
 
+const hasMenu = process.env.menu === 'true';
+const hasProjects = process.env.menu === 'true';
+const hasCompanies = process.env.menu === 'true';
+
+const getConfigData = (data) => {
+	return {
+		hasProjects,
+		hasCompanies,
+		activePage: data.page,
+		menu: hasMenu ? menu : [],
+	}
+}
+
 app.get("/", (req,res) => {
-	res.render('index', {
-		activePage: 'home',
-		menu: menu
-	});
+	res.render('index', getConfigData({
+		page: 'home'
+	}));
 });
 
 app.get("/resume", (req,res) => {
-	res.render('resume', {
-		activePage: 'resume',
-		menu: menu
-	});
+	res.render('resume', getConfigData({
+		page: 'resume'
+	}));
 })
 
 app.get("/contact", (req,res) => {
-	res.render('contact', {
-		activePage: 'contact',
-		menu: menu
-	});
+	res.render('contact', getConfigData({
+		page: 'contact'
+	}));
 })
 
 app.get("/login", (req,res) => {
-	res.render('login', {
-		activePage: 'login',
-		menu: menu
-	});
+	res.render('login', getConfigData({
+		page: 'login'
+	}));
 })
 
 app.get("*", (req,res) => {
